8d6715c7347fd1f8c1780b8d2d242b1f8ec898de
jllancafil <jorgellancafil@gmail.com>
2020-02-22 12:20:04 -0300

    init

---
+++ src/app/getinfo/getinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-getinfo',
+  templateUrl: './getinfo.component.html',
+  styleUrls: ['./getinfo.component.scss']
+})
+export class GetinfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,18 @@
+//importamos todo lo que vamos a usar
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"]
+})
+
+export class MainComponent {
+  constructor() {
+
+  }
+
+
+
+}
