f7b06af522f3359e6c03a8463c24c3595b06c9c5
jllancafil <jorgellancafil@gmail.com>
2020-02-22 15:22:46 -0300

    +

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { HistoryComponent } from './history/history.component';
 
 const routes: Routes = [
   { path: "main", component: MainComponent },
+  { path: "history", component: HistoryComponent },
   { path: "**", component: MainComponent }
 ];
 
